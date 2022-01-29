ad385545acde573da388f5c83567f82de8661c63
Kaustubh Solanki <kaustubhsolanki98@gmail.com>
2022-01-29 20:48:07 +0530

    Initial commit

--- Linked list/main.c
+++ Linked list/main.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node *create_list(struct node *start);
-struct node *insert_in_beginning(struct node *start, int data);
-void insert_at_end(struct node *start, int data);
-void display_list(struct node *start);
-void total_nodes(struct node *start);
-
 struct node
 {
     int data;
     struct node *next;
 };
 
+struct node *create_list(struct node *start);
+struct node *insert_at_start(struct node *start, int data);
+void insert_at_end(struct node *start, int data);
+void display_list(struct node *start);
+
 int main()
 {
     int choice;
     struct node *start = NULL;
     start = create_list(start);
-    while(1) {
+    while (1)
+    {
         printf("\n1. Display list.");
-        printf("\n2. Count total nodes");
-        printf("\n");
-        scanf("%d",&choice);
-        switch(choice) {
-            case 1: display_list(start); break;
-            case 2: total_nodes(start); break;
+        printf("\nEnter your choice");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            display_list(start);
+            break;
         }
     }
     return 0;
@@ -33,27 +34,24 @@ int main()
 
 struct node *create_list(struct node *start)
 {
-    printf("\nEnter total number of nodes: ");
-    int total_nodes, data;
+    int data, total_nodes;
+    printf("\nEnter total number of elements: ");
     scanf("%d", &total_nodes);
-    // Check if n is not less than equal to zero
     if (total_nodes <= 0)
-    {
         return start;
-    }
-    printf("\nEnter element 1: ");
+    printf("\nEnter value 1: ");
     scanf("%d", &data);
-    start = insert_in_beginning(start, data);
+    start = insert_at_start(start, data);
     for (int i = 2; i <= total_nodes; i++)
     {
-        printf("\nEnter element %d : ",i);
-        scanf("%d",&data);
+        printf("\nEnter value %d: ",i);
+        scanf("%d", &data);
         insert_at_end(start, data);
     }
     return start;
 }
 
-struct node *insert_in_beginning(struct node *start, int data)
+struct node *insert_at_start(struct node *start, int data)
 {
     struct node *temp;
     temp = (struct node *)malloc(sizeof(struct node));
@@ -63,36 +61,27 @@ struct node *insert_in_beginning(struct node *start, int data)
     return start;
 }
 
-void insert_at_end(struct node *start, int data) {
+void insert_at_end(struct node *start, int data)
+{
     struct node *temp, *p;
+    p = start;
+    while (p->next != NULL)
+        p = p->next;
     temp = (struct node *)malloc(sizeof(struct node));
     temp->data = data;
-    p = start;
-    while(p->next != NULL) {
-        p=p->next;
-    }
-    p->next=temp;
-    temp->next=NULL;
+    temp->next = NULL;
+    p->next = temp;
 }
 
-void display_list(struct node *start) {
+void display_list(struct node *start)
+{
     struct node *p;
     p = start;
-    printf("\n List is -> ");
-    while(p != NULL) {
+    printf("\nList is -> ");
+    while (p != NULL)
+    {
         printf("%d ", p->data);
-        p=p->next;
-    }
-    printf("\n");
-}
-
-void total_nodes(struct node *start) {
-    struct node *p;
-    int counter;
-    p = start;
-    while(p->next != NULL) {
-        counter++;
         p = p->next;
     }
-    printf("\nTotal number of nodes: %d",counter);
+    printf("\n");
 }
